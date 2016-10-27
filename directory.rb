08abd2f3275921a11c8e1446478f3d7bb1b14e05
Chris <chrixs99@hotmail.com>
2016-10-27 20:17:24 +0100

    exercise 12 added and removed repetitive code

--- directory.rb
+++ directory.rb
@@ -3,26 +3,17 @@ def input_students
   puts "Then add cohort, age, height and country of birth:"
   students = []
   name = gets.chop
-  cohort = gets.chomp
-  if cohort.empty?
-    cohort = "November"
-  end
-  age = gets.chomp
-  height = gets.chomp
-  country = gets.chomp
   while !name.empty? do
+    cohort = gets.chomp
+    if cohort.empty?
+      cohort = "November"
+    end
+    age = gets.chomp
+    height = gets.chomp
+    country = gets.chomp
     students << {name: name, cohort: cohort, age: age, height: height, country: country}
     puts "Now we have #{students.count} students"
     name = gets.chop
-    if !name.empty?
-      cohort = gets.chomp
-      if cohort.empty?
-        cohort = "November"
-      end
-      age = gets.chomp
-      height = gets.chomp
-      country = gets.chomp
-    end
   end
   students
 end
@@ -49,8 +40,10 @@ def cohort_print(students)
 end
 
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)".center(80)
+  if !students.empty?
+    students.each do |student|
+      puts "#{student[:name]} (#{student[:cohort]} cohort)".center(80)
+    end
   end
 end
 
@@ -65,8 +58,10 @@ end
 def print_footer(names)
   if names.count > 1
     puts "Overall, we have #{names.count} great students".center(82)
-  else
+  elsif names.count == 1
     puts "Overall, we have one great student".center(82)
+  else
+    puts "No students are enrolled in Villains Academy".center(82)
   end
 end
 
